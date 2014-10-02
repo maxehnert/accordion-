bc8178392f23528d112f13813050f46d713eae70
Max Ehnert <max.ehnert@gmail.com>
2014-10-02 16:23:57 -0400

    added stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log('The Iron Yard Rocks');
+// $('.a').click(function() {
+// console.log('test 2');
+// $('.content_one').removeClass('content');
+//     $( '.b' ).addClass('add-content');
+//
+//   });
+// console.log('test 1')
+
+
+$('.a').click(function(){
+
+  $(this).removeClass('content');
+
+  $('.c').addClass('b');
+
+});
